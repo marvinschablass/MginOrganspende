90647f860fb241627ff492135086ae818882877e
Marvin Schablaß <SCH19017@spengergasse.at>
2021-06-11 12:52:28 +0200

    Create Organspende.rb

---
+++ MGIN_Organspende/Organspende.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Organspende
+
+  def abfrage_patient_blutgruppe(blutgruppePatient)
+    if blutgruppePatient == "A+" or blutgruppePatient == "A-" or blutgruppePatient == "B+" or blutgruppePatient == "B-" or blutgruppePatient == "AB+" or blutgruppePatient == "AB-" or blutgruppePatient == "0+" or blutgruppePatient == "0-"
+      return true
+
+    else
+      puts"Keine gültige Blutgruppe"
+    end
+  end
+
+  def abfrage_spender_blutgruppe(blutgruppeSpender)
+    if blutgruppeSpender == "A+" or blutgruppeSpender == "A-" or blutgruppeSpender == "B+" or blutgruppeSpender == "B-" or blutgruppeSpender == "AB+" or blutgruppeSpender == "AB-" or blutgruppeSpender == "0+" or blutgruppeSpender == "0-"
+      return true
+
+    else
+      puts "Keine gültige Blutgruppe"
+    end
+  end
+
+  def kompatibilitaet_blutgruppe(blutgruppePatient, blutgruppeSpender)
+    if blutgruppePatient == blutgruppeSpender or blutgruppePatient == "0+" or blutgruppeSpender =="0+"
+      return true
+
+    else
+      puts "Leider nicht kompatibel"
+    end
+  end
+  end
+
