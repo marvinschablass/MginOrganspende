825928a914967f3970c9879cd4c6e6b3174df080
Marvin Schablaß <SCH19017@spengergasse.at>
2021-06-16 15:43:16 +0200

    GitHub Hooks

--- MGIN_Organspende/Test/test_organ_test.rb
+++ MGIN_Organspende/Test/test_organ_test.rb
@@ -7,7 +7,7 @@ class TestOrganTest < Minitest::Test
   def setup
     @organspende = Organspende.new
   end
-  def test_organspende
+  def test_organspende1
     assert_equal(true , @organspende.abfrage_patient_blutgruppe("A+"))
     #assert_equal true , true
   end
