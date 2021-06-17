a06c5f4f464b82c9fbd852a1e4700064f712ebf5
Celina <nel18281@spengergasse.at>
2021-06-17 09:54:42 +0200

    Erstellen der if-Abfrage zur Überprüfung der Gültigkeit der eingegbenen Blutgruppe

--- MGIN_Organspende/ablauf_skript.rb
+++ MGIN_Organspende/ablauf_skript.rb
@@ -23,3 +23,7 @@ SvnrPatient = gets.chomp
 puts "Geben Sie bitte die Blutgruppe des Patienten wie folgt ein (A+, B+, A-, B-, AB-, AB+, 0-, 0+)"
 
 blutgruppePatient = gets.chomp
+
+if organspende.abfrage_patient_blutgruppe(blutgruppePatient)
+
+end
