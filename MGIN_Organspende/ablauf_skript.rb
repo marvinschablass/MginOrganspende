5915fa40ae0c1e9ca8704501e91e8af23565b459
Celina <nel18281@spengergasse.at>
2021-06-17 09:49:31 +0200

    Erstellung der ersten Abfragen; Vorname und Nachname des Patienten in der Klasse ablauf_skript.rb.

--- MGIN_Organspende/ablauf_skript.rb
+++ MGIN_Organspende/ablauf_skript.rb
@@ -1,1 +1,13 @@
+# frozen_string_literal: true
 
+require_relative "./klasse_abfragen.rb"
+
+
+organspende =Organspende.new
+puts "Bitte geben Sie den Vornamen des Patienten ein."
+
+VornamePatient = gets.chomp
+
+puts"Bitte geben Sie den Nachnamen des Patienten ein."
+
+NachnamePatient = gets.chomp
