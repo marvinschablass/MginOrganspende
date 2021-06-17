d2b2cfb755dac637237bd63c60a9bcf8f617f912
Nourn123 <75122575+Nourn123@users.noreply.github.com>
2021-06-17 10:17:21 +0200

    Erstellung der Methode zur Überprüfung der Gültigkeit der Blutgruppeneingabe: Klasse Organ

--- MGIN_Organspende/klasse_abfragen.rb
+++ MGIN_Organspende/klasse_abfragen.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class KlasseAbfragen
+  def abfrage_patient_blutgruppe(blutgruppePatient)
+    if blutgruppePatient == "A+" or blutgruppePatient == "A-" or blutgruppePatient == "B+" or blutgruppePatient == "B-" or blutgruppePatient == "AB+" or blutgruppePatient == "AB-" or blutgruppePatient == "0+" or blutgruppePatient == "0-"
+      return true
+
+    else
+      puts"Keine gültige Blutgruppe"
+    end
+  end
 end
