4f506d7f4bd8cb14fdcd0397a0ec09312fcc31ef
Celina <nel18281@spengergasse.at>
2021-06-17 09:55:38 +0200

    Abfrage der Stammdaten des Patienten

--- MGIN_Organspende/ablauf_skript.rb
+++ MGIN_Organspende/ablauf_skript.rb
@@ -25,5 +25,23 @@ puts "Geben Sie bitte die Blutgruppe des Patienten wie folgt ein (A+, B+, A-, B-
 blutgruppePatient = gets.chomp
 
 if organspende.abfrage_patient_blutgruppe(blutgruppePatient)
+  puts "Bitte geben Sie den Vornamen des Spenders ein"
 
+  VornameSpender = gets.chomp
+
+  puts"Bitte geben Sie den Nachnamen des Spenders ein"
+
+  NachnameSpender = gets.chomp
+
+  puts"Bitte geben Sie das Alter des Spenders ein"
+
+  AlterSpender = gets.chomp
+
+  puts"Bitte geben Sie die Sozoialversicherungsnummer des Spenders ein"
+
+  SvnrSpender = gets.chomp
+
+  puts "Geben Sie bitte die Blutgruppe des Spenders wie folgt ein (A+, B+, A-, B-, AB-, AB+, 0-, 0+)"
+
+  blutgruppeSpender = gets.chomp
 end
