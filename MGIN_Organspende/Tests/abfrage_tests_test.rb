271621a41a9759b95c343ea47a91d8f26ac6d62a
Marvin Schablaß <SCH19017@spengergasse.at>
2021-06-17 09:40:19 +0200

    Erstellung der Testklasse

---
+++ MGIN_Organspende/Tests/abfrage_tests_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+
+class Abfrage_testsTest < Minitest::Unit::TestCase
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip 'Not implemented'
+  end
+end
