346417b5cf190527e18682ae6fd82ecfa14bb13f
Fabienne <sch19029@spengergasse.at>
2021-06-18 09:23:16 +0200

    Hinzufügen Kompatibilität

--- MGIN_Organspende/klasse_abfragen.rb
+++ MGIN_Organspende/klasse_abfragen.rb
@@ -17,7 +17,16 @@ class KlasseAbfragen
         puts"Keine gültige Blutgruppe"
 
       end
+    end
+
+    def kompatibilitaet_blutgruppe(blutgruppePatient, blutgruppeSpender)
+      if blutgruppePatient == blutgruppeSpender or blutgruppePatient == "0+" or blutgruppeSpender =="0+" or  blutgruppeSpender or blutgruppePatient == "A+" or blutgruppeSpender =="A-" or  blutgruppeSpender or blutgruppePatient == "A+" or blutgruppeSpender =="B+"
+        return true
+
+      else
+        puts "Leider nicht kompatibel"
+      end
+    end
   end
   end
 
-  end
