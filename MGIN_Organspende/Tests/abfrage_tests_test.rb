9d90165ef119ad528a55bbcd92cd73bacf31f1f5
Fabienne <sch19029@spengergasse.at>
2021-06-18 09:39:17 +0200

    Erstellung Test_organspend_numero_due

--- MGIN_Organspende/Tests/abfrage_tests_test.rb
+++ MGIN_Organspende/Tests/abfrage_tests_test.rb
@@ -13,5 +13,9 @@ class AbfrageTestsTest < Minitest::Unit::TestCase
     assert_equal(true , @organspende.kompatibilitaet_blutgruppe("A+", "A+"))
   end
 
+  def test_organspende1
+    assert_nil(@organspende.abfrage_patient_blutgruppe("Haus"))
+  end
+
 
 end
